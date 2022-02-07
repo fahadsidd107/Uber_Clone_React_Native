234d5060e070e1b5ce89c7aaac568a012a57aa20
fahadsidd107 <fsiddiqui107@gmail.com>
2022-02-07 20:08:42 +0500

    work complete

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,12 @@
 import { View, Text,SafeAreaView,StatusBar } from 'react-native';
 import React from 'react';
+import tw from 'tailwind-react-native-classnames';
 console.log(StatusBar.currentHeight);
 const HomeScreen = () => {
   return (
     <View style={{marginTop:StatusBar.currentHeight}}>
-      <Text>Home Screen</Text>
+    {/* agr {} k ander[] krke normal styling dena chahain to krste hain  */}
+      <Text style={tw`text-red-500 p-10`}>Home Screen</Text>
     </View>
   );
 };
