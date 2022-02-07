415866788aa2e8195fce68291cf9d40c48ca9d94
fahadsidd107 <fsiddiqui107@gmail.com>
2022-02-07 19:44:59 +0500

    work complete

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { View,StyleSheet } from 'react-native';
 import {Provider} from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
