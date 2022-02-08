f971e73b65fe87aa5eca81f0210c764509a28a0a
fahadsidd107 <fsiddiqui107@gmail.com>
2022-02-09 03:07:01 +0500

    work complete

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,37 +1,39 @@
-import { StyleSheet, Text, View,FlatList,TouchableOpacity } from 'react-native';
-import React from 'react';
+import {
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
 
 const data = [
   {
-    id:"123",
-    title: 'Get a ride',
-    image: 'https://links.papareact.com/3pn',
-    screen: 'MapScreen',
+    id: "123",
+    title: "Get a ride",
+    image: "https://links.papareact.com/3pn",
+    screen: "MapScreen",
   },
   {
-    id:"567",
-    title: 'Order Food',
-    image: 'https://links.papareact.com/28w',
-    screen: 'EatsScreen',
+    id: "567",
+    title: "Order Food",
+    image: "https://links.papareact.com/28w",
+    screen: "EatsScreen",
   },
 ];
 
 const NavOptions = () => {
   return (
-    <FlatList 
-    data={data}
-    horizontal
-    renderItem={({ item }) => {
-<TouchableOpacity>
-  <Text>
-    {item.image}
-  </Text>
-</TouchableOpacity>
-     } }/>
+    <FlatList
+      data={data}
+      horizontal
+      renderItem={({ item }) => {
+        <TouchableOpacity>
+          <Text>{item.image}</Text>
+        </TouchableOpacity>;
+      }}
+    />
   );
 };
 
 export default NavOptions;
-
-const styles = StyleSheet.create({});
 
