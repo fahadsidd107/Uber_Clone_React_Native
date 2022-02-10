077ae5d19d397599a1cd0e469a2b632ba422bcb6
fahadsidd107 <fsiddiqui107@gmail.com>
2022-02-11 00:16:28 +0500

    work complete

--- App.js
+++ App.js
@@ -2,17 +2,26 @@ import { StatusBar } from 'expo-status-bar';
 import { View,StyleSheet } from 'react-native';
 import {Provider} from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
+import EatsScreen from './screens/EatsScreen';
 import { store } from './store';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
+// import 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
     <Provider store={store}>
+    <NavigationContainer>
     <SafeAreaProvider>
-    <View>
-<HomeScreen/>
-    </View>
+    <Stack.Navigator>
+    <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown:false}}/>
+    <Stack.Screen name="MapScreen" component={MapScreen} options={{headerShown:false}}/>
+    <Stack.Screen name="EatsScreen" component={EatsScreen} options={{headerShown:false}}/>
+    </Stack.Navigator>
     </SafeAreaProvider>
+    </NavigationContainer>
     </Provider>
   );
 }
