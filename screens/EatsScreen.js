c99f89a83530070e2013ef8758566beab56648ff
fahadsidd107 <fsiddiqui107@gmail.com>
2022-02-12 19:46:17 +0500

    work complete

--- screens/EatsScreen.js
+++ screens/EatsScreen.js
@@ -1,9 +1,11 @@
-import { View, Text } from 'react-native'
+import { View, Text, StatusBar } from 'react-native'
 import React from 'react'
-
+console.log(StatusBar.currentHeight);
 const EatsScreen = () => {
   return (
-    <View>
+    <View
+    style={{marginTop:StatusBar.currentHeight}}
+    >
       <Text>EatsScreen</Text>
     </View>
   )
