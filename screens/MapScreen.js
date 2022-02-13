4e483a5bacd39db6c6108832b71c95ff2078b513
fahadsidd107 <fsiddiqui107@gmail.com>
2022-02-13 06:36:54 +0500

    work complete

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,12 +1,14 @@
 import { View, Text,StatusBar } from 'react-native'
 import React from 'react'
-
+import tw from 'tailwind-react-native-classnames';
+import Map from '../components/Map';
 const MapScreen = () => {
   return (
     <View
-    style={{marginTop:StatusBar.currentHeight}}
-    >
-      <Text>here is the map stuff</Text>
+    // style={{marginTop:StatusBar.currentHeight}}
+     >
+      <View style={tw`h-1/2`}><Map/></View>
+      <View style={tw`h-1/2`}></View>
     </View>
   )
 }
