fb384839d6ca71b38a195c619ed487a67c729091
fahadsidd107 <fsiddiqui107@gmail.com>
2022-02-13 06:11:18 +0500

    work complete

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,13 @@ import { View, Text,SafeAreaView,StatusBar,Image } from 'react-native';
 import React from 'react';
 import tw from 'tailwind-react-native-classnames';
 import NavOptions from '../components/NavOptions';
-console.log(StatusBar.currentHeight);
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from '@env';
+import{ useDispatch } from 'react-redux';
+import { setDestination, setOrigin } from '../';
+console.log(GOOGLE_MAPS_APIKEY);
 const HomeScreen = () => {
+  const dispatch = useDispatch();
   return (
     <View
      style={{marginTop:StatusBar.currentHeight}}
@@ -20,6 +25,30 @@ const HomeScreen = () => {
             uri:'https://links.papareact.com/gzs',
           }}
         />
+        <GooglePlacesAutocomplete
+      placeholder='Search your place'
+      styles={{
+        container: {
+          flex: 0,
+        },
+        textInput: {
+          fontSize: 14,
+        }
+      }}
+      onPress={(data, details = null) => {
+        console.log(data, details);
+      }}
+      fetchDetails={true}
+      returnKeyType={'search'}
+      enablePoweredByContainer={false}
+      minLength={2} // minimum length of text to search
+      query={{
+        key: GOOGLE_MAPS_APIKEY,
+        language: 'en',
+      }}
+      nearbyPlacesAPI='GooglePlacesSearch'
+      debounce={400}
+    />
         <NavOptions/>
       </View>
     </View>
