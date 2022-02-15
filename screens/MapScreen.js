f0896c7bc44c89a4e920eb9afc5276a247ccb32a
fahadsidd107 <fsiddiqui107@gmail.com>
2022-02-16 04:35:55 +0500

    work complete

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -2,13 +2,19 @@ import { View, Text,StatusBar } from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames';
 import Map from '../components/Map';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StackActions } from '@react-navigation/native';
 const MapScreen = () => {
   return (
     <View
     // style={{marginTop:StatusBar.currentHeight}}
      >
       <View style={tw`h-1/2`}><Map/></View>
-      <View style={tw`h-1/2`}></View>
+      <View style={tw`h-1/2`}>
+        <Stack.Navigator>
+          <Stack.Screen name="NavigationCard" component={NavigationCard} options={{headerShown:false}}/>
+        </Stack.Navigator>
+      </View>
     </View>
   )
 }
