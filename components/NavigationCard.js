efb2ac714900bded87f81a2814e3a223fafcf320
fahadsidd107 <fsiddiqui107@gmail.com>
2022-02-16 04:42:26 +0500

    work complete

---
+++ components/NavigationCard.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const NavigationCard = () => {
+  return (
+    <View>
+      <Text>NavigationCard</Text>
+    </View>
+  )
+}
+
+export default NavigationCard
+
+const styles = StyleSheet.create({})
