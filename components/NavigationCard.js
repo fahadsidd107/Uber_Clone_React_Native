53d80c35d3a859b93cb373e70ca13229d215b7c9
fahadsidd107 <fsiddiqui107@gmail.com>
2022-02-17 18:49:50 +0500

    work complete

--- components/NavigationCard.js
+++ components/NavigationCard.js
@@ -1,17 +1,17 @@
-import { StyleSheet, Text, View,SafeAreaView } from 'react-native'
-import React from 'react'
-import tw from 'tailwind-react-native-classnames';
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import React from "react";
+import tw from "tailwind-react-native-classnames";
 const NavigationCard = () => {
   return (
-  <SafeAreaView style={tw`bg-white flex-1`}>
-  <Text style={tw`text-center py-5 text-xl`}>Good Morning , Fahad</Text>
-  <View>
-    
-  </View>
-  </SafeAreaView>
-  )
-}
+    <SafeAreaView style={tw`bg-white flex-1`}>
+      <Text style={tw`text-center py-5 text-xl`}>Good Morning , Fahad</Text>
+      <View style={tw`border-t border-gray-200 flex-shrink`}>
+        <View></View>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default NavigationCard
+export default NavigationCard;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
