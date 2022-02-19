f9f5f5570ef07ecd3ba580579986de867703bdc6
fahadsidd107 <fsiddiqui107@gmail.com>
2022-02-19 18:29:43 +0500

    work complete

--- components/NavigationCard.js
+++ components/NavigationCard.js
@@ -12,7 +12,7 @@ const NavigationCard = () => {
 
   return (
     <SafeAreaView style={tw`bg-white flex-1`}>
-      <Text style={tw`text-center py-5 text-xl`}>Good Morning , Fahad</Text>
+      <Text style={tw`text-center py-5 text-xl`}>Good Day , Fahad</Text>
       <View style={tw`border-t border-gray-200 flex-shrink`}>
         <View>
  <GooglePlacesAutocomplete 
