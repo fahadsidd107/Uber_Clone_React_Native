98679a530df981bd6b485c21759a660e0877cd65
fahadsidd107 <fsiddiqui107@gmail.com>
2022-02-21 05:11:41 +0500

    work complete

--- components/NavFavourites.js
+++ components/NavFavourites.js
@@ -1,6 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
+const data=[
+    {
+        id:123,
+        icon:'home',
+        location:'Home',
+        destination: 'Code Street, London, UK',
+    }
+]
+
 const NavFavourites = () => {
   return (
     <View>
