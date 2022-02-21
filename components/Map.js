45c4d599efb005b5a2a967a36c5e34b1c8b883bf
fahadsidd107 <fsiddiqui107@gmail.com>
2022-02-21 05:03:29 +0500

    work complete

--- components/Map.js
+++ components/Map.js
@@ -15,9 +15,10 @@ const Map = () => {
 
    useEffect(()=>{
 if (!origin && !destination) return;
-mapRef.current.fitToSuppliedMarkers([origin,destination])
-
-
+mapRef.current.fitToSuppliedMarkers([origin,destination], {
+    edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+    animated: true,
+    });
    },[ origin,destination])
     
   return (
