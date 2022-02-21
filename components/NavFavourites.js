c8899b9bf5c65f330ed0697ae4564535edd796bd
fahadsidd107 <fsiddiqui107@gmail.com>
2022-02-22 01:11:10 +0500

    work complete

--- components/NavFavourites.js
+++ components/NavFavourites.js
@@ -1,36 +1,49 @@
-import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { Icon } from 'react-native-elements';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import { Icon } from "react-native-elements";
+import tw from "tailwind-react-native-classnames";
 
-const data=[
-    {
-        id:123,
-        icon:'home',
-        location:'Home',
-        destination: 'Code Street, London, UK',
-    },
-    {
-        id:456,
-        icon:'breifcase',
-        location:'Work',
-        destination: 'London Eye, London, UK',
-    }
-]
+const data = [
+  {
+    id: 123,
+    icon: "home",
+    location: "Home",
+    destination: "Code Street, London, UK",
+  },
+  {
+    id: 456,
+    icon: "briefcase",
+    location: "Work",
+    destination: "London Eye, London, UK",
+  },
+];
 
 const NavFavourites = () => {
   return (
     <FlatList
       data={data}
       keyExtractor={(item) => item.id}
-      renderItem={({ item }) => ( 
-       <TouchableOpacity>
-<Text>data</Text>
-       </TouchableOpacity>
+      renderItem={({ item : {location,destination,icon} }) => (
+        <TouchableOpacity style={tw`flex-row item-center p-5`}>
+          <Icon
+            style={tw`mr-4 rounded-full bg-gray-300 p-3`}
+            name={icon}
+            type="ionicon"
+            color="white"
+            size={18}
+          />
+        </TouchableOpacity>
       )}
     />
-  )
-}
+  );
+};
 
-export default NavFavourites
+export default NavFavourites;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
