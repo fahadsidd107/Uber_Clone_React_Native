270441c289a4901aa2bd1d87cbe8bcfa8c3c2990
fahadsidd107 <fsiddiqui107@gmail.com>
2022-02-24 01:17:06 +0500

    work complete

--- App.js
+++ App.js
@@ -15,11 +15,13 @@ export default function App() {
     <Provider store={store}>
     <NavigationContainer>
     <SafeAreaProvider>
+    <KeyboardAvoidingView>
     <Stack.Navigator>
     <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown:false}}/>
     <Stack.Screen name="MapScreen" component={MapScreen} options={{headerShown:false}}/>
     <Stack.Screen name="EatsScreen" component={EatsScreen} options={{headerShown:false}}/>
     </Stack.Navigator>
+    </KeyboardAvoidingView>
     </SafeAreaProvider>
     </NavigationContainer>
     </Provider>
