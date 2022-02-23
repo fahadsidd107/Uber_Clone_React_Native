8423422da74d3c6d613dd6e1d24a7d49027ab20c
fahadsidd107 <fsiddiqui107@gmail.com>
2022-02-24 01:29:21 +0500

    work complete

--- components/NavigationCard.js
+++ components/NavigationCard.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, SafeAreaView, TextInput } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, TextInput,TouchableOpacity } from "react-native";
 import React from "react";
 import tw from "tailwind-react-native-classnames";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
@@ -57,6 +57,11 @@ const NavigationCard = () => {
           />
         </View>
         <NavFavourites />
+      </View>
+      <View>
+        <TouchableOpacity>
+
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
