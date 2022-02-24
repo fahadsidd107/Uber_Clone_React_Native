f76cf0bbfb3f51d4288036feda3b2a4ecea2446e
fahadsidd107 <fsiddiqui107@gmail.com>
2022-02-24 23:55:03 +0500

    wirk complete

--- components/NavigationCard.js
+++ components/NavigationCard.js
@@ -61,7 +61,7 @@ const NavigationCard = () => {
       </View>
       <View style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-200`}>
         <TouchableOpacity 
-        onPress={() => navigation.navigate("")}
+        onPress={() => navigation.navigate("RideOptionCard")}
         style={tw`flex flex-row justify-between bg-black w-24 px-4 py-3 rounded-full`}>
 <Icon name='car' type='font-awesome' color='#fff' size={16} />
 <Text style={tw`text-white text-center`}>Rides</Text>
