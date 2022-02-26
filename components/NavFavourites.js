7a07dd886f7e07a3fe13c331b4a305d1f00ff9ea
fahadsidd107 <fsiddiqui107@gmail.com>
2022-02-27 04:58:46 +0500

    work complete

--- components/NavFavourites.js
+++ components/NavFavourites.js
@@ -51,7 +51,7 @@ const NavFavourites = () => {
           />
           <View>
             <Text style={tw`font-semibold text-lg`}>{location}</Text>
-            <View style={{padding:10}}>
+            <View style={{paddingRight:6}}>
             <Text style={tw`text-gray-500`}>{destination}</Text>
             </View>
           </View>
