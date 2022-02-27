eda4daa91656ea12b0a85bc3d2f868e384dd5747
fahadsidd107 <fsiddiqui107@gmail.com>
2022-02-27 05:10:14 +0500

    work complete

--- components/RideOptionCard.js
+++ components/RideOptionCard.js
@@ -1,10 +1,22 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
 import tw from 'tailwind-react-native-classnames';
+import { Icon } from "react-native-elements";
 const RideOptionCard = () => {
   return (
     <View style={tw`bg-white flex-grow`}>
+    <View>
+<TouchableOpacity>
+<Icon
+            style={tw`mr-4 rounded-full bg-gray-300 p-3`}
+            name='chevron-left'
+            type="fontawesome"
+            color="white"
+            size={18}
+          />
+</TouchableOpacity>
       <Text style={tw`text-center my-5 text-xl`}>Select Your Ride</Text>
+    </View>
     </View>
   );
 };
