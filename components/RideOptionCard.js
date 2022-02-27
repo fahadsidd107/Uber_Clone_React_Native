b8327b1468a0c522ec98af9354bc19e25cccbc31
fahadsidd107 <fsiddiqui107@gmail.com>
2022-02-27 05:16:21 +0500

    work complete

--- components/RideOptionCard.js
+++ components/RideOptionCard.js
@@ -2,11 +2,16 @@ import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
 import tw from 'tailwind-react-native-classnames';
 import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
+
 const RideOptionCard = () => {
+  const navigation = useNavigation();
   return (
     <View style={tw`bg-white flex-grow`}>
     <View>
-<TouchableOpacity style={tw`absolute top-3 left-5 p-3 rounded-full`} >
+<TouchableOpacity 
+onPress={()=>{}}
+style={tw`absolute top-3 left-5 p-3 rounded-full bg-red-200`} >
 <Icon
             name='chevron-left'
             type="fontawesome"
