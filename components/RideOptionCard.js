d330c037f350ad7e3ea6687faa8aeb03e16797ae
fahadsidd107 <fsiddiqui107@gmail.com>
2022-03-01 01:09:27 +0500

    work complete

--- components/RideOptionCard.js
+++ components/RideOptionCard.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity,FlatList,Image } from "react-native";
 import React from "react";
 import tw from 'tailwind-react-native-classnames';
 import { Icon } from "react-native-elements";
@@ -15,13 +15,13 @@ const data =[
     id:"Uber-XL-456",
     title:"Uber XL",
     multiplier:1.2,
-    imagw:"https://links.papareact.com/3pn"
+    imagw:"https://links.papareact.com/5w8"
   },
   {
     id:"Uber-LUX-789",
     title:"Uber LUX",
-    multiplier:"1",
-    imagw:"https://links.papareact.com/3pn"
+    multiplier:1.75,
+    imagw:"https://links.papareact.com/7pf"
   },
 ]
 const RideOptionCard = () => {
@@ -36,6 +36,7 @@ style={tw`absolute top-3 left-5 p-3 z-50 rounded-full bg-black`} >
 </TouchableOpacity>
       <Text style={tw`text-center my-5 text-xl`}>Select Your Ride</Text>
     </View>
+    <FlatList data={data}/>
     </View>
   );
 };
