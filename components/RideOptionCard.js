9fd5ea1a37e1272ea8b2a05215d7fdba2b6afcdf
fahadsidd107 <fsiddiqui107@gmail.com>
2022-03-02 07:41:11 +0500

    work complete

--- components/RideOptionCard.js
+++ components/RideOptionCard.js
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View, TouchableOpacity,FlatList,Image } from "react-native";
-import React from "react";
+import React,{ useState }  from "react";
 import tw from 'tailwind-react-native-classnames';
 import { Icon } from "react-native-elements";
 import { useNavigation } from "@react-navigation/native";
@@ -9,23 +9,24 @@ const data =[
     id:"Uber-X-123",
     title:"UberX",
     multiplier:1,
-    imagw:"https://links.papareact.com/3pn"
+    image:"https://links.papareact.com/3pn"
   },
   {
     id:"Uber-XL-456",
     title:"Uber XL",
     multiplier:1.2,
-    imagw:"https://links.papareact.com/5w8"
+    image:"https://links.papareact.com/5w8"
   },
   {
     id:"Uber-LUX-789",
     title:"Uber LUX",
     multiplier:1.75,
-    imagw:"https://links.papareact.com/7pf"
+    image:"https://links.papareact.com/7pf"
   },
 ]
 const RideOptionCard = () => {
   const navigation = useNavigation();
+  const [selected,setSelected] = useState(null);
   return (
     <View style={tw`bg-white flex-grow`}>
     <View>
@@ -38,11 +39,21 @@ style={tw`absolute top-3 left-5 p-3 z-50 rounded-full bg-black`} >
     </View>
     <FlatList data={data} 
       keyExtractor={(item)=>item.id}
-      renderItem={({item})=>(
-       <TouchableOpacity>
-         <Text>
-           Fahad
-         </Text>
+      renderItem={({item : {id ,title ,multiplier ,image},item})=>(
+       <TouchableOpacity style={tw`flex-row justify-between items-center px-10`}>
+       <Image
+         style={{
+           width:100,
+            height:100,
+            resizeMode:'contain',
+         }}
+         source={{uri:image}}
+       />
+       <View style={tw`-ml-6`}>
+         <Text style={tw`text-xl font-semibold`}>{title}</Text>
+         <Text>Travel Time...</Text>
+       </View>
+       <Text style={tw`text-xl`}>$99</Text>
        </TouchableOpacity> 
       )}
     />
