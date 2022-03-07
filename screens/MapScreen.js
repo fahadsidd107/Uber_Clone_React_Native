a590ad8957aa5af4a7dce4ad7ea96a6fa91d8bfb
fahadsidd107 <fsiddiqui107@gmail.com>
2022-03-07 12:31:50 +0500

    work complete

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,4 +1,4 @@
-import { View, Text,StatusBar } from 'react-native'
+import { View, Text,StatusBar,TouchableOpacity } from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames';
 import Map from '../components/Map';
@@ -7,12 +7,18 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StackActions } from '@react-navigation/native';
 import NavigationCard from '../components/NavigationCard';
 import RideOptionCard from '../components/RideOptionCard';
+import {Icon} from 'react-native-elements/dist/icons/Icon';
+import { useNavigation } from '@react-navigation/native';
 const MapScreen = () => {
   const Stack = createNativeStackNavigator();
+  const navigation = useNavigation();
   return (
     <View
     // style={{marginTop:StatusBar.currentHeight}}
      >
+     <TouchableOpacity style={tw`bg-gray-100 absolute top-8 left-4 z-50 p-3 rounded-full shadow-lg`}>
+<Icon name='menu'/>
+     </TouchableOpacity>
       <View style={tw`h-1/2`}><Map/></View>
       <View style={tw`h-1/2`}>
         <Stack.Navigator>
